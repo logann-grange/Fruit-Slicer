d73b0bbfa5bd7b9bdb46a53c0213686759a3e38e
logann-grange <logann.grange@laplateforme.io>
2026-01-28 13:41:19 +0100

    ajout sons freeze, explosion et coupe

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ from bombe import Bombe
 from glaçon import Glaçon
 
 pygame.init()
+pygame.mixer.init()
 
 screen = pygame.display.set_mode((1000, 750))
 font = pygame.font.SysFont('Arial', 40, bold=True)
@@ -32,63 +33,67 @@ coord_boom = None
 def print_boom(boom, coord_boom) :
     if boom :
         screen.blit(img_boom, coord_boom)
+        pygame.mixer.Sound("assets/sons/boom.wav").play()
+
 
 while running:
     #clock.tick(60)
-    
-    screen.blit(background, (0, 0))
-    
-    list_object, time_move = movement.move(list_object, time_move, freeze)
-    list_object, time_pop, list_used = movement.pop(list_object, time_pop, freeze, list_used)
-    print_boom(boom, coord_boom)
+    if not stop :
+        screen.blit(background, (0, 0))
+        list_object, time_move = movement.move(list_object, time_move, freeze)
+        list_object, time_pop, list_used = movement.pop(list_object, time_pop, freeze, list_used)
 
-    # Gestion des événements
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        # Gestion des événements
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        if event.type == pygame.KEYDOWN:
-            # Vérifier si la touche correspond à un objet
-            for i in range(len(list_object) - 1, -1, -1):
-                object = list_object[i]
-                if event.unicode.lower() == object.touche.lower():
-                    # Retirer la touche de list_used si elle y est
-                    if object.touche in list_used:
-                        list_used.remove(object.touche)
+            if event.type == pygame.KEYDOWN:
+                # Vérifier si la touche correspond à un objet
+                for i in range(len(list_object) - 1, -1, -1):
+                    object = list_object[i]
+                    if event.unicode.lower() == object.touche.lower():
+                        pygame.mixer.Sound("assets/sons/coupe.wav").play()
+                        # Retirer la touche de list_used
+                        if object.touche in list_used:
+                            list_used.remove(object.touche)
 
-                    list_object.pop(i)
-                    
-                    if isinstance(object, Glaçon):
-                        freeze = True
-                        time_freeze = datetime.now()
+                        list_object.pop(i)
 
-                    if isinstance(object, Bombe) :
-                        boom = True
-                        coord_boom = (object.coord_x-(object.size/2 + 100/2), object.coord_y-(object.size/2 + 100/2)) # 100/2 correspond à la moitier de la taille de img_boom
-                    break
+                        if isinstance(object, Glaçon):
+                            freeze = True
+                            time_freeze = datetime.now()
+                            pygame.mixer.Sound("assets/sons/glace.wav").play()
 
-    # Supprimer les objets hors écran et afficher
-    for i in range(len(list_object) - 1, -1, -1):
-        object = list_object[i]
-        
-        if object.coord_y > 750:
-            life = logic.strike(object, life)
-            print(life)
-            # Retirer la touche de list_used quand l'objet sort
-            if object.touche in list_used:
-                list_used.remove(object.touche)
-            list_object.pop(i)
-        else:
-            screen.blit(object.image, (object.coord_x, object.coord_y))
-            screen.blit(font.render(object.touche.upper(), 1, (0, 0, 0)), (object.coord_x + 20, object.coord_y - 30))
+                        if isinstance(object, Bombe) :
+                            boom = True
+                            coord_boom = (object.coord_x-(object.size/2 + 100/2), object.coord_y-   (object.size/2 + 100/2)) # 100/2 correspond à la moitier de la taille   de img_boom
+                        break
 
-    freeze, time_freeze = movement.freezer(freeze, time_freeze, screen, background_freeze)
+        # Supprimer les objets hors écran
+        for i in range(len(list_object) - 1, -1, -1):
+            object = list_object[i]
 
-    if logic.defaite(life, boom) : #vérifie si la partie est perdue
-        life = 0
-        #pygame.quit() # à modifier par une fenêtre de défaite
+            if object.coord_y > 750:
+                life = logic.strike(object, life)
+                print(life)
+                # Retirer la touche de list_used quand l'objet sort
+                if object.touche in list_used:
+                    list_used.remove(object.touche)
+                list_object.pop(i)
+            else:
+                screen.blit(object.image, (object.coord_x, object.coord_y))
+                screen.blit(font.render(object.touche.upper(), 1, (0, 0, 0)), (object.coord_x + 20, object.coord_y - 30))
+
+        freeze, time_freeze = movement.freezer(freeze, time_freeze, screen, background_freeze)
+
+        if logic.defaite(life, boom) : #vérifie si la partie est perdue
+            life = 0
+            stop = True
+            #pygame.quit() # à modifier par une fenêtre de défaite
+        print_boom(boom, coord_boom)
 
 
+        #pygame.display.flip()
     pygame.display.flip()
-
 pygame.quit()
